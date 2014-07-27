9f4ebd1db7e27a2343130f9effeab27393095a4d
peteruithoven <peter@peteruithoven.nl>
2014-07-27 21:40:12 +0200

    Include existing segments
    And skip straight lines

--- spec/spec/svg.topath.js
+++ spec/spec/svg.topath.js
@@ -13,16 +13,22 @@ describe('toPoly()', function() {
 		}
 		var path;
 		var path2;
+		var curvedPath;
     beforeEach(function() {
       path = draw.path(pathData);
       path2 = draw.path(pathData+"Z");
+			curvedPath = draw.path('M34.5,45.5 c0,0,38-43,71-18 s31,81 20,78 c0,0,38-43,71-18 s31,81-20,78 -16-47-16-47');
     })
+		
 
     it('generates a polyline', function() {
       expect(path.toPoly() instanceof SVG.Polyline).toBe(true)
     })
 		it('generates a polygon', function() {
       expect(path2.toPoly() instanceof SVG.Polygon).toBe(true)
+    })
+		it('generates a polyline from curved path', function() {
+			expect(curvedPath.toPoly() instanceof SVG.Polyline).toBe(true)
     })
 		
   })
--- svg.topoly.js
+++ svg.topoly.js
@@ -8,36 +8,94 @@
       var total, point, distance
         , length = 0
         , points = []
+				, segments;
 
       // parse sample value
-      sample = new SVG.Number(sample || '1%')
-
+      sample = new SVG.Number(sample || '1%');
       // render data
       SVG.parser.path.setAttribute('d', this.toString())
-
       // get total length
       total = SVG.parser.path.getTotalLength()
-
+			
       // calculate sample distance
-      distance = sample.unit == '%' ?
-        total * sample.value : // sample distance in %
-      sample.unit == 'px' ?
-        sample.value :         // fixed sample distance in px
-        total / sample.value   // specific number of samples
-
-      // gather points
+			if(sample.unit === '%') {
+					distance = total * sample.value; // sample distance in %
+			} else if (sample.unit === 'px') {
+				sample.value; // fixed sample distance in px
+			} else {
+				total / sample.value; // specific number of samples
+			}
+			
+			segments = this.value;
+			
+			var addPoint = function(x,y) {
+				var lastPoint = points[points.length-1];
+				// When the last point doesn't equal the current point add the current point
+				if (!lastPoint || x!=lastPoint[0] || y!=lastPoint[1]) {
+					points.push([x,y]);
+				}
+			}
+			var addSegmentPoint = function(segment){
+				var x, y, type = segment[0];
+				if(type === "Z") return;
+				switch(type) {
+					case "M":
+					case "L":
+					case "T":
+						x = segment[1];
+						y = segment[2];
+						break;
+					case "H":
+						x = segment[1];
+						break;
+					case "V":
+						y = segment[1];
+						break;
+					case "C":
+						x = segment[5];
+						y = segment[6];
+						break;
+					case "S":
+					case "Q":	
+						x = segment[3];
+						y = segment[4];
+						break;
+				}
+				addPoint(x,y);
+			}
+		
+      // sample through path
+			var lastSegment, segmentIndex;
       while (length < total) {
-        point = SVG.parser.path.getPointAtLength(length)
-        points.push([ point.x, point.y ])
-
+				segmentIndex = SVG.parser.path.getPathSegAtLength(length);
+				var segment = segments[segmentIndex];
+				// new segment? 
+				if (segment != lastSegment){
+					// add the segment we just left
+					if(lastSegment !== undefined)	addSegmentPoint(lastSegment);
+					lastSegment = segment;
+				}
+	
+				// Add points in between when curving
+				switch(segment[0]) {
+						case "C":
+						case "T":
+						case "S":
+						case "Q":	
+							point = SVG.parser.path.getPointAtLength(length);
+							addPoint(point.x,point.y);
+							break;
+				}
+        
         // increment by sample value
-        length += distance
+        length += distance;
       }
-
-      // add last point
-      point = SVG.parser.path.getPointAtLength(total)
-      points.push([ point.x, point.y ])
-
+			
+			// add remaining segments we didn't pass while sampling
+			for (var i=segmentIndex,len=segments.length;i<len;++i) {
+				addSegmentPoint(segments[i]);
+			}
+  		
       // send out as point array
       return new SVG.PointArray(points)
     }
